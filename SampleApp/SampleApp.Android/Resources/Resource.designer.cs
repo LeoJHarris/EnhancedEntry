7c675eb4a4271d46296b8d1d01b78d98ad68ac09
Leonard <leonard.harris.xeniasuite@gmail.com>
2018-03-25 12:03:12 +1300

    Removed ref Xamarin.Android.Support.Annotations.27.0.2

--- SampleApp/SampleApp.Android/Resources/Resource.designer.cs
+++ SampleApp/SampleApp.Android/Resources/Resource.designer.cs
@@ -4289,8 +4289,26 @@ namespace SampleApp.Droid
 			// aapt resource value: 0x7f020055
 			public const int avd_hide_password = 2130837589;
 			
+			// aapt resource value: 0x7f020136
+			public const int avd_hide_password_1 = 2130837814;
+			
+			// aapt resource value: 0x7f020137
+			public const int avd_hide_password_2 = 2130837815;
+			
+			// aapt resource value: 0x7f020138
+			public const int avd_hide_password_3 = 2130837816;
+			
 			// aapt resource value: 0x7f020056
 			public const int avd_show_password = 2130837590;
+			
+			// aapt resource value: 0x7f020139
+			public const int avd_show_password_1 = 2130837817;
+			
+			// aapt resource value: 0x7f02013a
+			public const int avd_show_password_2 = 2130837818;
+			
+			// aapt resource value: 0x7f02013b
+			public const int avd_show_password_3 = 2130837819;
 			
 			// aapt resource value: 0x7f020057
 			public const int design_bottom_navigation_item_background = 2130837591;
